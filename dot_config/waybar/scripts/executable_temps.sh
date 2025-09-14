2a63b42cab0ad3c0fd7b9748275960df320d2aa0
Matt Edmonston <matt.edmonston@gmail.com>
2025-09-14 07:44:27 +0100

    Power menu and temp tweaks.

--- dot_config/waybar/scripts/executable_temps.sh
+++ dot_config/waybar/scripts/executable_temps.sh
@@ -10,9 +10,36 @@ for zone in /sys/class/thermal/thermal_zone*; do
   [ -e "$zone/temp" ] || continue
   name=$(cat "$zone/type" 2>/dev/null || echo "zone")
   temp=$(awk '{printf "%d", $1/1000}' "$zone/temp")
-  tooltip+="$name: ${temp}°C\n"
+  tooltip+=$(printf "%3d°C | %s" "$temp" "$name")
+  tooltip+="\n"
 
   if (( $(echo "$temp > $max_temp" | bc -l) )); then
+    max_temp=$temp
+  fi
+done
+
+for temp_file in /sys/class/hwmon/hwmon*/temp*_input; do
+  [ -e "$temp_file" ] || continue
+
+  dir="${temp_file%/*}"
+  if [ -r "$dir/name" ]; then
+    name=$(cat "$dir/name" 2>/dev/null || echo "hwmon")
+  else
+    name="hwmon"
+  fi
+
+  label_file="${temp_file%_input}_label"
+  if [ -r "$label_file" ]; then
+    label=$(cat "$label_file" 2>/dev/null || echo "sensor")
+  else
+    label=$(basename "${temp_file%_*}")
+  fi
+
+  temp=$(awk '{printf "%d", $1/1000}' "$temp_file")
+  tooltip+=$(printf "%3d°C | %s (%s)" "$temp" "$label" "$name")
+  tooltip+="\n"
+
+  if (( temp > max_temp )); then
     max_temp=$temp
   fi
 done
