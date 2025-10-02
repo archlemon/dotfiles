82ab8b59ab7e399b0b6cd226163ebb960dbbf0ca
Matt Edmonston <matt.edmonston@gmail.com>
2025-10-02 23:20:44 +0100

    Adds user.js for firefox.

---
+++ dot_config/firefox-userjs/user.js
@@ -0,0 +1,6 @@
+// about:config to manually set
+// about:support > Application Basics > Profile Directory
+user_pref("browser.startup.page", 0);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.sessionstore.resume_from_crash", false);
+user_pref("browser.sessionstore.resume_session_once", false);
